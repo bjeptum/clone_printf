b102a89071d0411ebeeb456b71b6ac13f899aee7
b_jeptum <brendajeptum@gmail.com>
2022-10-19 11:40:41 -0700

    update main

--- main.h
+++ main.h
@@ -12,5 +12,11 @@ int _printf(const char *format, ...);
 int handler(const char *str, va_list list);
 int percent_handler(const char *str, va_list list, int *i);
 
+/* utilities.c */
+int _strlen(char *s);
 
+/* putchar.c */
+
+int _putchar(char c);
+int buffer(char c);
 #endif/* MAIN_H */

8e1fae6fb6f67c27440c985ce9ad0032519e9cdf
b_jeptum <brendajeptum@gmail.com>
2022-10-19 11:25:51 -0700

    Utilities support for function

--- main.h
+++ main.h
@@ -1,6 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdlib.h>
+#include <stdarg.h>
+
+/* 0-printf.c */
+
 int _printf(const char *format, ...);
+
+/* handler.c */
+int handler(const char *str, va_list list);
+int percent_handler(const char *str, va_list list, int *i);
+
 
 #endif/* MAIN_H */
---
+++ utilities.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/*
+ *  _strlen - returns the length of a string
+ *  @s: string inputted
+ *  Return: length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+	
+	for (; *s != '\0'; s++)
+	{
+		i++;
+	}
+	return (i);
+}

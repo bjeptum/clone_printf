0854cb614b0ea39e750663eb5f9f2963cce602e7
b_jeptum <brendajeptum@gmail.com>
2022-10-21 02:56:48 -0700

    Update main.h

--- main.h
+++ main.h
@@ -15,17 +15,25 @@ int percent_handler(const char *str, va_list list, int *i);
 
 /* utilities.c */
 int _strlen(const char *s);
+char *itoa(long int, int);
 
 /* putchar.c */
 
 int _putchar(char c);
 int buffer(char c);
 
-/* print character */
+/* printf handling format specifiers */
 int print_char(va_list list);
-
-/* print string */
 int print_string(va_list list);
+int print_integer(va_list);
+int print_binary(va_list);
+int print_rot(va_list);
+int print_unsigned(va_list);
+int print_octal(va_list);
+int print_hexadecimal_low(va_list);
+int print_hexadecimal_upp(va_list);
+int print_pointer(va_list);
+int print_rev_string(va_list);
 
 /**
  * struct _format - Typedef struct
